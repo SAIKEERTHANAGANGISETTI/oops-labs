1e5233b18bd559dff421e0a06d4cdefd06c66137
SAIKEERTHANAGANGISETTI <159503239+SAIKEERTHANAGANGISETTI@users.noreply.github.com>
2024-03-09 22:10:39 +0530

    Add files via upload

---
+++ diamond.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class ClassA
+{
+    public:
+    void aClass()
+    {
+    cout<<"Iam a ClassA\n";
+    }
+};
+class ClassB: public ClassA
+{
+    public:
+    void bClass()
+    {
+    cout<<"Iam a ClassB\n";
+    }
+};
+class ClassC: public ClassA
+{
+    public:
+    void cClass()
+    {
+    cout<<"Iam a ClassC\n";
+    }
+};
+class ClassD: public ClassC,public ClassB
+{
+    public:
+    void dClass()
+    {
+    cout<<"Iam a ClassD\n";
+    }
+};
+int main()
+{
+    ClassD obj1;
+    //obj1 couldn't access aClass method as it throws ambiguity while calling.
+    obj1.bClass();
+    obj1.cClass();
+    obj1.dClass();
+}
